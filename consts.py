a9248d14f5a82c5b757a2d1c906263debeee5047
Xi LIU <e0385051@u.nus.edu>
2021-09-09 16:24:17 +0800

    added momentum to real space transformation

--- consts.py
+++ consts.py
@@ -12,7 +12,7 @@ N = 2 ** 10
 xc = (2 * N - 1) / 2
 # xc=101
 # width
-sgm = 15
+sgm = 50
 
 
 # parameters for coeffcient u,v w
@@ -35,13 +35,14 @@ def w(t):
 omega = 0.03
 # tilt strength
 omegaF = 0.05
-T = 2 * np.pi / omega
-Q = 2 ** 18
-tTot = 48* T
+T = 2* np.pi / omega
+Q = 2 ** 14
+tTot = 3*T
 dt = tTot / Q
 
 # init gaussian part of the wavefunction
-wvFcnt = [np.exp(-(n - xc) ** 2 / (4 * sgm ** 2)) for n in range(0, 2 * N)]
+# wvFcnt = [np.exp(-(n - xc) ** 2 / (4 * sgm ** 2)) for n in range(0, 2 * N)]
+
 # plt.figure()
 # plt.plot(wvFcnt)
 # plt.show()
@@ -55,19 +56,54 @@ EVAL=np.sqrt(AVal**2+BVal**2+CVal**2)
 # v1=[iB-A, C+E] without normalization const
 v10=1j*BVal-AVal
 v11=CVal+EVAL
+uA=v10
+uB=v11
 
 
-for j in range(0, N):
-    wvFcnt[2 * j] *= v10
-    wvFcnt[2 * j + 1] *= v11
-# normalization const
-c02Tmp = 0
-for elem in wvFcnt:
-    c02Tmp += np.abs(elem) ** 2
-C0 = np.sqrt(c02Tmp)
+# for j in range(0, N):
+#     wvFcnt[2 * j] *= v10
+#     wvFcnt[2 * j + 1] *= v11
+# # normalization const
+# c02Tmp = 0
+# for elem in wvFcnt:
+#     c02Tmp += np.abs(elem) ** 2
+# C0 = np.sqrt(c02Tmp)
+#
+# psi0 = []
+# for n in range(0, 2 * N):
+#     psi0.append(wvFcnt[n] * np.exp(1j * n * k0) / C0)
 
-psi0 = []
-for n in range(0, 2 * N):
-    psi0.append(wvFcnt[n] * np.exp(1j * n * k0) / C0)
 
-L = len(psi0)
+
+#########################################
+def l2Norm(vec):
+    tmp=0
+    for j in range(0,len(vec)):
+        tmp+=np.abs(vec[j])**2
+    return np.sqrt(tmp)
+j0=xc
+kIndAll=list(range(0,2*N))
+kValsAll=[2*np.pi*kIndTmp/(2*N) for kIndTmp in kIndAll]
+fValsAll=[np.exp(-(kValsAll[kIndTmp]+k0)**2*sgm**2+1j*j0*(kValsAll[kIndTmp]+k0)) for kIndTmp in kIndAll]
+psi0=np.zeros(2*N,dtype=complex)
+
+for n in range(0,N):
+    #psi2n
+    psi2nVal=0
+    for kIndTmp in kIndAll:
+        psi2nVal+=fValsAll[kIndTmp]*np.exp(-1j*kValsAll[kIndTmp]*2*n)
+
+    psi0[2*n]=psi2nVal*uA
+    #psi2n+1
+    psi2np1Val=0
+    for kIndTmp in kIndAll:
+        psi2np1Val+=fValsAll[kIndTmp]*np.exp(-1j*kValsAll[kIndTmp]*(2*n+1))
+    psi0[2*n+1]=psi2np1Val*uB
+
+psi0/=l2Norm(psi0)
+print(np.max(np.abs(psi0))**2/np.min(np.abs(psi0))**2)
+plt.figure()
+plt.plot(range(0,2*N),np.abs(psi0)**2,color="black")
+plt.show()
+
+
